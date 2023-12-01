4edb2e8fb58d1314a9322cb2b8d282dcc126167a
Bilal Abouzid <imbilalabouzid@gmail.com>
2023-12-02 00:33:18 +0200

    Add login page & Add fonts

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import Home from "../Components/Home";
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/home" exact component={null} />
+      <Route path="/home" exact component={Home} />
       <Redirect to="/home" />
     </Switch>
   );
