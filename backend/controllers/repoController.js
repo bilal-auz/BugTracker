e5975a54fc86868131a72151cbe52adde1a9a503
Bilal Abouzid <imbilalabouzid@gmail.com>
2023-12-15 16:51:56 +0200

    Refactor repoController.js to handle JWT authentication

--- backend/controllers/repoController.js
+++ backend/controllers/repoController.js
@@ -1,14 +1,17 @@
 const axios = require("axios");
+const jwt = require("jsonwebtoken");
 
 //https://api.github.com/orgs/ORG/repos
 const getRepos = async (req, res) => {
   try {
-    const access_token = req.headers.Authorization;
+    const access_token = req.headers.authorization.split(" ")[1];
+
+    const decoded = jwt.verify(access_token, process.env.JWT_SECRET);
 
     const config = {
       headers: {
         Accept: "application/json",
-        Authorization: `${access_token}`,
+        Authorization: `Bearer ${decoded.access_token}`,
         "X-GitHub-Api-Version": "2022-11-28",
       },
     };
@@ -20,9 +23,9 @@ const getRepos = async (req, res) => {
 
     res.status(200).send(data);
   } catch (error) {
-    const { status, data } = error.response;
+    // const { status, data } = error.response;
 
-    res.status(status).send(data.message);
+    res.status(400).send(error.message);
   }
 };
 
