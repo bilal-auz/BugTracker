88189e380b57c7f11bc6427f65887294c2f5411b
Bilal Abouzid <imbilalabouzid@gmail.com>
2023-12-28 23:33:44 +0200

    Handle if no repos

--- frontend/src/Components/dashboard/pages/Profile.jsx
+++ frontend/src/Components/dashboard/pages/Profile.jsx
@@ -197,112 +197,121 @@ function Profile() {
               <span className="text-xs text-[#00000070]">-top 4-</span>
             </p>
             <div className="flex flex-row justify-between items-stretch flex-wrap w-full">
-              {repos.slice(0, 4).map((repo, index) => (
-                <div
-                  key={index}
-                  className={`flex flex-col justify-between w-[49%] px-4 py-4 border-2 rounded-md transtransition-all duration-200 hover:scale-105 ${
-                    index > 1 && "mt-4"
-                  }`}
-                >
-                  <div>
-                    <p className="text-left text-lg font-bold capitalize mb-2">
-                      <svg
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 inline mr-1"
-                      >
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g
-                          id="SVGRepo_tracerCarrier"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></g>
-                        <g id="SVGRepo_iconCarrier">
-                          {" "}
-                          <path
-                            d="M19.8978 16H7.89778C6.96781 16 6.50282 16 6.12132 16.1022C5.08604 16.3796 4.2774 17.1883 4 18.2235"
-                            stroke="#000000"
-                            stroke-width="1.5"
-                          ></path>{" "}
-                          <path
-                            d="M8 7H16"
-                            stroke="#000000"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                          ></path>{" "}
-                          <path
-                            d="M8 10.5H13"
-                            stroke="#000000"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                          ></path>{" "}
-                          <path
-                            d="M13 16V19.5309C13 19.8065 13 19.9443 12.9051 20C12.8103 20.0557 12.6806 19.9941 12.4211 19.8708L11.1789 19.2808C11.0911 19.2391 11.0472 19.2182 11 19.2182C10.9528 19.2182 10.9089 19.2391 10.8211 19.2808L9.57889 19.8708C9.31943 19.9941 9.18971 20.0557 9.09485 20C9 19.9443 9 19.8065 9 19.5309V16.45"
-                            stroke="#000000"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                          ></path>{" "}
-                          <path
-                            d="M10 22C7.17157 22 5.75736 22 4.87868 21.1213C4 20.2426 4 18.8284 4 16V8C4 5.17157 4 3.75736 4.87868 2.87868C5.75736 2 7.17157 2 10 2H14C16.8284 2 18.2426 2 19.1213 2.87868C20 3.75736 20 5.17157 20 8M14 22C16.8284 22 18.2426 22 19.1213 21.1213C20 20.2426 20 18.8284 20 16V12"
-                            stroke="#000000"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                          ></path>{" "}
-                        </g>
-                      </svg>
-                      {repo?.name}
-                    </p>
-                    <p
-                      className={`text-base text-left ${
-                        !repo?.description && "italic text-[#00000096]"
-                      }`}
-                    >
-                      {repo?.description || "No Descrption..."}
-                    </p>
-                  </div>
-                  <div className="flex flex-row justify-between w-full mt-5">
-                    <p
-                      className={`text-left text-base font-semibold ${
-                        !repo?.primaryLanguage?.name &&
-                        "italic text-[#00000096]"
-                      }`}
-                    >
-                      <svg
-                        className="w-3 mr-2 inline fill-current"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
-                        stroke-linejoin="round"
-                        stroke-miterlimit="2"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        style={{
-                          color: repo?.primaryLanguage?.color || "#00000096",
-                        }}
-                      >
-                        <circle
-                          // fill="#8B0000"
-                          cx="11.998"
-                          cy="11.998"
-                          fill-rule="nonzero"
-                          r="9.998"
-                        />
-                      </svg>
-                      {repo?.primaryLanguage?.name || "No Primary Language..."}
-                    </p>
-                    <a href={repo?.url} target="_blank">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                      </svg>
-                    </a>
-                  </div>
+              {repos.length > 0 ? (
+                <div className="w-full">
+                  <p className="text-base font-bold text-[#0000006e] italic">
+                    you don't have any pinned repos...
+                  </p>
                 </div>
-              ))}
+              ) : (
+                repos.slice(0, 4).map((repo, index) => (
+                  <div
+                    key={index}
+                    className={`flex flex-col justify-between w-[49%] px-4 py-4 border-2 rounded-md transtransition-all duration-200 hover:scale-105 ${
+                      index > 1 && "mt-4"
+                    }`}
+                  >
+                    <div>
+                      <p className="text-left text-lg font-bold capitalize mb-2">
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="w-5 inline mr-1"
+                        >
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></g>
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M19.8978 16H7.89778C6.96781 16 6.50282 16 6.12132 16.1022C5.08604 16.3796 4.2774 17.1883 4 18.2235"
+                              stroke="#000000"
+                              stroke-width="1.5"
+                            ></path>{" "}
+                            <path
+                              d="M8 7H16"
+                              stroke="#000000"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                            ></path>{" "}
+                            <path
+                              d="M8 10.5H13"
+                              stroke="#000000"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                            ></path>{" "}
+                            <path
+                              d="M13 16V19.5309C13 19.8065 13 19.9443 12.9051 20C12.8103 20.0557 12.6806 19.9941 12.4211 19.8708L11.1789 19.2808C11.0911 19.2391 11.0472 19.2182 11 19.2182C10.9528 19.2182 10.9089 19.2391 10.8211 19.2808L9.57889 19.8708C9.31943 19.9941 9.18971 20.0557 9.09485 20C9 19.9443 9 19.8065 9 19.5309V16.45"
+                              stroke="#000000"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                            ></path>{" "}
+                            <path
+                              d="M10 22C7.17157 22 5.75736 22 4.87868 21.1213C4 20.2426 4 18.8284 4 16V8C4 5.17157 4 3.75736 4.87868 2.87868C5.75736 2 7.17157 2 10 2H14C16.8284 2 18.2426 2 19.1213 2.87868C20 3.75736 20 5.17157 20 8M14 22C16.8284 22 18.2426 22 19.1213 21.1213C20 20.2426 20 18.8284 20 16V12"
+                              stroke="#000000"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                            ></path>{" "}
+                          </g>
+                        </svg>
+                        {repo?.name}
+                      </p>
+                      <p
+                        className={`text-base text-left ${
+                          !repo?.description && "italic text-[#00000096]"
+                        }`}
+                      >
+                        {repo?.description || "No Descrption..."}
+                      </p>
+                    </div>
+                    <div className="flex flex-row justify-between w-full mt-5">
+                      <p
+                        className={`text-left text-base font-semibold ${
+                          !repo?.primaryLanguage?.name &&
+                          "italic text-[#00000096]"
+                        }`}
+                      >
+                        <svg
+                          className="w-3 mr-2 inline fill-current"
+                          clip-rule="evenodd"
+                          fill-rule="evenodd"
+                          stroke-linejoin="round"
+                          stroke-miterlimit="2"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                          style={{
+                            color: repo?.primaryLanguage?.color || "#00000096",
+                          }}
+                        >
+                          <circle
+                            // fill="#8B0000"
+                            cx="11.998"
+                            cy="11.998"
+                            fill-rule="nonzero"
+                            r="9.998"
+                          />
+                        </svg>
+                        {repo?.primaryLanguage?.name ||
+                          "No Primary Language..."}
+                      </p>
+                      <a href={repo?.url} target="_blank">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
           </div>
 
